2ce29023638a22a87c75fa29e3b529fbc752932b
Neil Ravikumar <mailneilr@gmail.com>
2026-09-21 19:40:45 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        /*
+        std::unordered_map<char, int> freqCountsS;
+        std::unordered_map<char, int> freqCountsT;
+        for (int i = 0; i < s.length(); i++) {
+            if(freqCountsS.contains(s[i])) {
+                freqCountsS[s[i]] += 1;
+                continue;
+            }
+            freqCountsS[s[i]] = 1;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if(freqCountsT.contains(t[i])) {
+                freqCountsT[t[i]] += 1;
+                continue;
+            }
+            freqCountsT[t[i]] = 1;
+        }
+        return freqCountsS == freqCountsT;
+        */
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        return s == t;
+
+    }
+};
